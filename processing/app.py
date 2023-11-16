d1c8f072ae044612843d7c0b289d5df1578b1589
ahammermuller <alineh@gmail.com>
2023-11-15 20:58:46 -0800

    debug process

--- processing/app.py
+++ processing/app.py
@@ -54,7 +54,7 @@ def populate_stats():
 
 
     # Get current datetime
-    old_datetime = stats['last_timestamp']
+    old_datetime = str(stats['last_timestamp'])
     current_timestamp = str(datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"))
 
     print(f"Old Timestamp: {old_datetime}")
